7f57055c437afd47510ad4351f2ce4139b0ac611
Georges Le Vaillant <g.p.levaillant@gmail.com>
2018-11-18 04:21:09 +0000

    8th Commit - Array changed to hash with symbols for name/cohort, added cohort info, also redid methods to work with hash

--- directory.rb
+++ directory.rb
@@ -1,19 +1,19 @@
 
 
 students = [
-  "Dr. Hannibal Lector", 
-  "Darth Vadar",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex Delarge",
-  "The Wicked witch of the south-west",
-  "Terminator",
-  "Freddy Krueger",
-  "Joker",
-  "Joffrey Baratheon",
-  "Norman Bates",
-  "Jason Vorhees",
-  "Michael Myers" 
+  {:name => "Dr. Hannibal Lector", :cohort => "november"}, 
+  {:name => "Darth Vadar", :cohort => "november"},
+  {:name => "Nurse Ratched", :cohort => "november"},
+  {:name => "Michael Corleone", :cohort => "november"},
+  {:name => "Alex Delarge", :cohort  => "november"},
+  {:name => "The Wicked witch of the south-west", :cohort => "november"},
+  {:name => "Terminator", :cohort => "november"},
+  {:name => "Freddy Krueger", :cohort => "november"},
+  {:name => "Joker", :cohort => "november"},
+  {:name => "Joffrey Baratheon", :cohort => "november"},
+  {:name => "Norman Bates", :cohort => "november"},
+  {:name => "Jason Vorhees", :cohort => "november"},
+  {:name => "Michael Myers", :cohort => "november"} 
   ]
 def print_header  
   puts "The students of villains academy"
@@ -22,7 +22,7 @@ end
 
 def print_names(names)
   names.each do |student|
-    puts student
+    puts "#{student[:name]} #{student[:cohort]} cohort)"
   end
 end
 
