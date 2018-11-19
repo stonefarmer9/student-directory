291cec5917a604c1e5aaf4f58a399b6410d0731e
Georges Le Vaillant <g.p.levaillant@gmail.com>
2018-11-19 03:32:07 +0000

    10th commit: Added interactive menu, can also now take cohort on entry of student info

--- directory.rb
+++ directory.rb
@@ -1,53 +1,88 @@
-
-
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Add students to evil register".center(75)
+    puts "2. Show all the evil students".center(75)
+    puts "9. Exit the EvIl app".center(75)
+  
+    selection = gets.chomp
+    case selection 
+      when "1"
+        students = input_students
+      when "2"
+        print_header
+        print_names(students)
+        print_footer(students)
+      when "9"
+        exit 
+      else
+        puts "Incorrect input, please try again"
+    end
+  end 
+end 
+=begin
 students = [
-  {name: "Dr. Hannibal Lector", cohort: :november}, 
-  {name:  "Darth Vadar", cohort: :november},
-  {name:  "Nurse Ratched", cohort: :november},
-  {name:  "Michael Corleone", cohort: :november},
-  {name:  "Alex Delarge", cohort: :november},
-  {name:  "The Wicked witch of the south-west", cohort: :november},
-  {name:  "Terminator", cohort: :november},
-  {name:  "Freddy Krueger", cohort: :november},
-  {name:  "Joker", cohort: :november},
-  {name:  "Joffrey Baratheon", cohort:  :november},
-  {name:  "Norman Bates", cohort: :november},
-  {name: "Jason Vorhees", cohort: :november},
-  {name:  "Michael Myers", cohort: :november} 
+  {name: "Dr. Hannibal Lector", cohort: :november, age: 49}, 
+  {name:  "Darth Vadar", cohort: :november, age: 35},
+  {name:  "Nurse Ratched", cohort: :november, age: 12},
+  {name:  "Michael Corleone", cohort: :november, age: 69},
+  {name:  "Alex Delarge", cohort: :november, age: 72},
+  {name:  "The Wicked witch of the south-west", cohort: :november, age: 152},
+  {name:  "Terminator", cohort: :november, age: 1},
+  {name:  "Freddy Krueger", cohort: :november, age: 45},
+  {name:  "Joker", cohort: :november, age: 36},
+  {name:  "Joffrey Baratheon", cohort:  :november, age: 2},
+  {name:  "Norman Bates", cohort: :november, age: 29},
+  {name: "Jason Vorhees", cohort: :november, age: 18},
+  {name:  "Michael Myers", cohort: :november, age: 562} 
   ]
+=end   
 def print_header  
-  puts "The students of villains academy"
-  puts "--------------------------------"     
+  puts "The students of villains academy".center(75)
+  puts "--------------------------------".center(75)     
 end
 
 def print_names(names)
-  names.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end
+    names.each_with_index do |student,index|
+      puts "[#{index + 1}]. #{student[:name]} (#{student[:age]}) (#{student[:cohort]} cohort)".center(75)
+    end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great students".center(75)
 end
 
 def input_students
-    puts "Please enter the names of new students"
-    puts "To finish press enter twice"
+    puts "Please enter the names of new students, hit return and enter the age, then hit return and enter the cohort".center(75)
+    puts "To finish press enter twice".center(75)
     
     students = []
     
     name = gets.chomp
-    
+    age = gets.chomp.to_i
+    cohort = gets.chomp.to_sym
     while !name.empty? do
-        students << {name: name, cohort: :november}
-        puts "Now we have #{students.count} students"
-        name = gets.chomp
+        students << {name: name, cohort: cohort, age: age}
+        
+        if students.count == 1
+          puts "Now we have #{students.count} student".center(75)
+          name = gets.chomp
+          age = gets.chomp.to_i
+          cohort = gets.chomp.to_sym
+        else
+          puts "Now we have #{students.count} students".center(75)
+          name = gets.chomp
+          age = gets.chomp.to_i
+          cohort = gets.chomp.to_sym
+        end 
     end 
     students
 end 
-students = input_students
 
-print_header
-print_names(students)
-print_footer(students)
+#students = input_students
+
+interactive_menu
+
+
+
 
